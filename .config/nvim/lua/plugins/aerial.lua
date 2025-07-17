ce7d851ff49affa834fd7d2193bf63ea240a0050
Jonathan Paul <jonpaul@deloitte.com>
2025-07-17 14:39:24 -0400

    nvim: reset { bind

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -12,8 +12,8 @@ return {
       -- optionally use on_attach to set keymaps when aerial has attached to a buffer
       on_attach = function(bufnr)
         -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+        -- vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        -- vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
       end,
       layout = {
         min_width = 30,
