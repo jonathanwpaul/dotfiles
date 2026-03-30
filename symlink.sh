ff19c67b3846c0650e08d29d615a78659495409f
Jonathan Paul <jonpaul@deloitte.com>
2026-03-30 14:04:26 -0400

    update symlink script for config dir

--- symlink.sh
+++ symlink.sh
@@ -1,16 +1,26 @@
 ignore_list=("init.sh" "symlink.sh" ".git" ".gitignore")
 
+# Directories whose children should be symlinked one level deeper
+# e.g. dotfiles/.config/opencode -> ~/.config/opencode
+nested_dirs=(".config")
+
 for file in $(ls -A ~/dotfiles); do
 	if ! [[ " ${ignore_list[*]} " =~ " $file " ]]; then
 		src="$HOME/dotfiles/$file"
 		dest="$HOME/$file"
-		if [ -d "$src" ]; then
+		if [[ " ${nested_dirs[*]} " =~ " $file " ]]; then
+			# For nested dirs, symlink each child into the corresponding $HOME subdir
 			mkdir -p "$dest"
 			for child in $(ls -A "$src"); do
-				ln -s "$src/$child" "$dest/$child"
+				ln -sf "$src/$child" "$dest/$child"
+			done
+		elif [ -d "$src" ]; then
+			mkdir -p "$dest"
+			for child in $(ls -A "$src"); do
+				ln -sf "$src/$child" "$dest/$child"
 			done
 		else
-			ln -s "$src" "$dest"
+			ln -sf "$src" "$dest"
 		fi
 	fi
 done
