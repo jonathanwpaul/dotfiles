221a76e8111276a9c6740fd0a35d55c6d18fba8c
Jonathan <paulwjonathan@gmail.com>
2026-04-25 15:25:25 -0400

    better binds for opencode

--- .config/nvim/lua/plugins/opencode.lua
+++ .config/nvim/lua/plugins/opencode.lua
@@ -35,22 +35,17 @@ return {
     vim.o.autoread = true -- Required for `opts.events.reload`
 
     -- Recommended/example keymaps
-    vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
-      require('opencode').ask('@this: ', { submit = true })
-    end, { desc = 'Ask opencode…' })
-    vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
+    vim.keymap.set({ 'n', 'x' }, '<leader>oca', function()
       require('opencode').select()
     end, { desc = 'Execute opencode action…' })
-    vim.keymap.set({ 'n', 't' }, '<C-.>', function()
+
+    vim.keymap.set({ 'n', 't' }, '<leader>oc', function()
       require('opencode').toggle()
     end, { desc = 'Toggle opencode' })
 
-    vim.keymap.set({ 'n', 'x' }, 'go', function()
-      return require('opencode').operator '@this '
-    end, { desc = 'Add range to opencode', expr = true })
-    vim.keymap.set('n', 'goo', function()
-      return require('opencode').operator '@this ' .. '_'
-    end, { desc = 'Add line to opencode', expr = true })
+    vim.keymap.set({ 'n', 'x' }, '<leader>oc?', function()
+      require('opencode').ask('@this: ', { submit = true })
+    end, { desc = 'Ask opencode…' })
 
     vim.keymap.set('n', '<S-C-u>', function()
       require('opencode').command 'session.half.page.up'
@@ -58,9 +53,5 @@ return {
     vim.keymap.set('n', '<S-C-d>', function()
       require('opencode').command 'session.half.page.down'
     end, { desc = 'Scroll opencode down' })
-
-    -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
-    vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment under cursor', noremap = true })
-    vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement under cursor', noremap = true })
   end,
 }
