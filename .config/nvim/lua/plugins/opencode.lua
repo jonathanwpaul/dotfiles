5e50efc8c1446eb7f0645826d01529731e67aa3a
Jonathan Paul <jonpaul@deloitte.com>
2026-06-25 11:31:12 -0400

    fix oc binds

--- .config/nvim/lua/plugins/opencode.lua
+++ .config/nvim/lua/plugins/opencode.lua
@@ -39,19 +39,12 @@ return {
       require('opencode').select()
     end, { desc = 'Execute opencode action…' })
 
-    vim.keymap.set({ 'n', 't' }, '<leader>oc', function()
+    vim.keymap.set({ 'n', 'x' }, '<leader>oct', function()
       require('opencode').toggle()
-    end, { desc = 'Toggle opencode' })
+    end, { desc = 'Execute opencode action…' })
 
-    vim.keymap.set({ 'n', 'x' }, '<leader>oc?', function()
+    vim.keymap.set({ 'n', 'x' }, '<leader>oc', function()
       require('opencode').ask('@this: ', { submit = true })
     end, { desc = 'Ask opencode…' })
-
-    vim.keymap.set('n', '<S-C-u>', function()
-      require('opencode').command 'session.half.page.up'
-    end, { desc = 'Scroll opencode up' })
-    vim.keymap.set('n', '<S-C-d>', function()
-      require('opencode').command 'session.half.page.down'
-    end, { desc = 'Scroll opencode down' })
   end,
 }
