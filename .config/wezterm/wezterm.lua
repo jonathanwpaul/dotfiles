21b020cfdb6e3005ed27a3e20cc7e52ee7ee002d
Jonathan Paul <jonpaul@deloitte.com>
2026-07-09 12:26:22 -0400

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "rose-pine-moon"
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
+config.font_size = 20.0
+config.macos_window_background_blur = 50
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
+return config
